a580f1b425fc0f36b28aeeb8d964531d9a245892
Samuel Hatcliff <samuel.hatcliff@gmail.com>
2022-08-14 17:58:37 -0600

    added more tests

--- tests.test.js
+++ tests.test.js
@@ -6,6 +6,25 @@ const request = require("supertest");
 const app = require("./app");
 const db = require("./db");
 
+let book_isbn;
+
+
+beforeEach(async () => {
+    let result = await db.query(`
+    INSERT INTO
+      books (isbn, amazon_url,author,language,pages,publisher,title,year)
+      VALUES(
+        '111112345',
+        'https://amazon.com/book',
+        'Mike',
+        'English',
+        300,
+        'scholastic',
+        'cool book', 2020)
+      RETURNING isbn`);
+
+    book_isbn = result.rows[0].isbn
+});
 
 describe("POST /books", function () {
     test("Adds a new book", async function () {
@@ -24,7 +43,7 @@ describe("POST /books", function () {
         expect(response.statusCode).toBe(201);
         expect(response.body.book).toHaveProperty("isbn");
     });
-
+    //tests required
     test("Prevents creating book without title", async function () {
         const response = await request(app)
             .post(`/books`)
@@ -101,6 +120,7 @@ describe("POST /books", function () {
             });
         expect(response.statusCode).toBe(400);
     });
+
     test("Prevents creating book without publisher", async function () {
         const response = await request(app)
             .post(`/books`)
@@ -145,118 +165,83 @@ describe("POST /books", function () {
             });
         expect(response.statusCode).toBe(400);
     });
-
-    describe("POST /books", function () {
-
-
-        test("Prevents creating book with string pages", async function () {
-            const response = await request(app)
-                .post(`/books`)
-                .send({
-                    isbn: '90123456',
-                    amazon_url: "https://amazon.com",
-                    author: "samuel",
-                    language: "english",
-                    publisher: "amazon books",
-                    pages: "150",
-                    title: "this is a title",
-                    year: 2022
-                });
-            expect(response.statusCode).toBe(400);
-        });
-
-        test("Prevents creating book with string year", async function () {
-            const response = await request(app)
-                .post(`/books`)
-                .send({
-                    isbn: '90123456',
-                    amazon_url: "https://amazon.com",
-                    author: "samuel",
-                    language: "english",
-                    publisher: "amazon books",
-                    pages: 150,
-                    title: "this is a title",
-                    year: "2022"
-                });
-            expect(response.statusCode).toBe(400);
-        });
-
-
-
-        describe("PUT /books/:id", function () {
-            test("Updates a single book", async function () {
-                const response = await request(app)
-                    .put(`/books/${book_isbn}`)
-                    .send({
-                        amazon_url: "https://taco.com",
-                        author: "mctest",
-                        language: "english",
-                        pages: 1000,
-                        publisher: "yeah right",
-                        title: "UPDATED BOOK",
-                        year: 2000
-                    });
-                expect(response.body.book).toHaveProperty("isbn");
-                expect(response.body.book.title).toBe("UPDATED BOOK");
+    //tests types
+    test("Prevents creating book with string pages", async function () {
+        const response = await request(app)
+            .post(`/books`)
+            .send({
+                isbn: '90123456',
+                amazon_url: "https://amazon.com",
+                author: "samuel",
+                language: "english",
+                publisher: "amazon books",
+                pages: "150",
+                title: "this is a title",
+                year: 2022
             });
+        expect(response.statusCode).toBe(400);
+    });
 
-            test("Prevents a bad book update", async function () {
-                const response = await request(app)
-                    .put(`/books/${book_isbn}`)
-                    .send({
-                        isbn: "32794782",
-                        badField: "DO NOT ADD ME!",
-                        amazon_url: "https://taco.com",
-                        author: "mctest",
-                        language: "english",
-                        pages: 1000,
-                        publisher: "yeah right",
-                        title: "UPDATED BOOK",
-                        year: 2000
-                    });
-                expect(response.statusCode).toBe(400);
-            })
-
-            describe("PUT /books/:id", function () {
-                test("Updates a single book", async function () {
-                    const response = await request(app)
-                        .put(`/books/${book_isbn}`)
-                        .send({
-                            amazon_url: "https://taco.com",
-                            author: "mctest",
-                            language: "english",
-                            pages: 1000,
-                            publisher: "yeah right",
-                            title: "UPDATED BOOK",
-                            year: 2000
-                        });
-                    expect(response.body.book).toHaveProperty("isbn");
-                    expect(response.body.book.title).toBe("UPDATED BOOK");
-                });
-
-                test("Prevents a bad book update", async function () {
-                    const response = await request(app)
-                        .put(`/books/${book_isbn}`)
-                        .send({
-                            isbn: "32794782",
-                            badField: "DO NOT ADD ME!",
-                            amazon_url: "https://taco.com",
-                            author: "mctest",
-                            language: "english",
-                            pages: 1000,
-                            publisher: "yeah right",
-                            title: "UPDATED BOOK",
-                            year: 2000
-                        });
-                    expect(response.statusCode).toBe(400);
-                })
+    test("Prevents creating book with string year", async function () {
+        const response = await request(app)
+            .post(`/books`)
+            .send({
+                isbn: '90123456',
+                amazon_url: "https://amazon.com",
+                author: "samuel",
+                language: "english",
+                publisher: "amazon books",
+                pages: 150,
+                title: "this is a title",
+                year: "2022"
             });
+        expect(response.statusCode).toBe(400);
+    });
+});
 
-            afterEach(async function () {
-                await db.query("DELETE FROM BOOKS");
+describe("PUT /books/:id", function () {
+    test("Updates a single book", async function () {
+        const response = await request(app)
+            .put(`/books/${book_isbn}`)
+            .send({
+                amazon_url: "https://amazon.com",
+                author: "samuel",
+                language: "english",
+                publisher: "amazon books",
+                pages: 150,
+                title: "this is a new title",
+                year: "2022"
             });
+        expect(response.body.book).toHaveProperty("isbn");
+        expect(response.body.book.title).toBe("this is a new title");
+    })
+});
 
-
-            afterAll(async function () {
-                await db.end()
+describe("PUT /books/:id", function () {
+    test("fails to update isbn", async function () {
+        const response = await request(app)
+            .put(`/books/${book_isbn}`)
+            .send({
+                isbn: '90153556',
+                amazon_url: "https://amazon.com",
+                author: "samuel",
+                language: "english",
+                publisher: "amazon books",
+                pages: 150,
+                title: "this is a new title",
+                year: "2022"
             });
+        expect(response.statusCode).toBe(400);
+    })
+});
+
+afterEach(async function () {
+    await db.query("DELETE FROM BOOKS");
+});
+
+
+afterAll(async function () {
+    await db.end()
+})
+
+
